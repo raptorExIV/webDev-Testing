24c982fb31d36811220150f1861853cafdcd17d6
raptor-x-z <raptorstrike04@gmail.com>
2023-09-17 02:45:46 +0800

    170923
    Added prototype codes to allow for change in <div> content depending on the option selected. Practical use will be to change the style of how the content will be displayed using HTML and JavaScript.

---
+++ index.js
@@ -0,0 +1,73 @@
+var formatA = false;
+var formatB = true;
+
+function checkingFormats() {
+
+    if (formatA) {
+        console.log("Format A present.");
+
+        document.getElementById("navigationBarBlk").innerHTML =
+
+        '<table class="tableNav" style="width:100%; table-layout: fixed;">' +
+
+            '<tr class="tableNav">' +
+
+                '<td style="padding: 0%; width: 55px; height: 55px;"><img src="images/rEXiV-2.png"> </td>' +
+
+                '<td style="padding-left: 15px; padding-right: 15px;">Brendan Tan Yuan Xi</td>' +
+
+                '<td class="active tableBtn"><a href="">Home</a></td>' +
+
+                '<td class="tableBtn"><a href="index2-platform.html">Delta</a></td>' +
+
+                '<td class="tableBtn"><a href="">Echo</a></td>' +
+
+                '<td class="tableBtn"><a href="">Proto-One</a></td>' +
+
+            '</tr>' +
+
+        '</table>'
+    }
+
+    else if (formatB) {
+        console.log("Format B present.");
+
+        document.getElementById("navigationBarBlk").innerHTML =
+
+        '<table class="tableNav" style="width:100%; table-layout: fixed;">' +
+
+            '<tr class="tableNav">' +
+
+                '<td style="padding: 0%; width: 55px; height: 55px;"><img src="images/rEXiV-2.png"> </td>' +
+
+                '<td style="padding-left: 15px; padding-right: 15px;">Brendan Tan Yuan Xi</td>' +
+
+                '<td class="active tableBtn"><a href="">rXz</a></td>' +
+
+                '<td class="tableBtn"><a href="index2-platform.html">RZII</a></td>' +
+
+                '<td class="tableBtn"><a href="">AxIOn</a></td>' +
+
+                '<td class="tableBtn"><a href="">Orion</a></td>' +
+
+            '</tr>' +
+
+        '</table>'
+        
+    }
+}
+
+function switchFormat() {
+
+    if (formatA && !formatB) {
+        formatA = false;
+        formatB = true;
+    }
+
+    else if (formatB && !formatA) {
+        formatB = false;
+        formatA = true;
+    }
+
+    checkingFormats();
+}
